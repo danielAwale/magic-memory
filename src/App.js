49857bf0bceeea5a0be01a8d52e2aa97492dc75a
danielAwale <danielmadhukarawale@gmail.com>
2022-09-25 16:20:48 -0700

    contain a card Images array of objects that we will sort and map over through states and functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import React, { useState } from 'react';
 import './App.css';
 
+// create a constant called cardImages to with an array of objects of our images!
+
+
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" },
+]
+
 function App() {
+
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //shuffle cards!
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages] //shuffledCards will take duplicate the cardimages array!
+      .sort(() => Math.random() - 0.5) //will sort it randomly 
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  }
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card =>
+        (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt="card front" />
+              <img className='back' src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
